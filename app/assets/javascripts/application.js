fe99dd429078e15a2d0b418b74ddb47a21d2f676
Ben Wexler <mixedywex@gmail.com>
2018-09-25 14:28:16 -0400

    Midi is working and with beat repeat

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,13 +37,13 @@ let tempoVal = 8;
 
 function setNoteDivision() {
 
-	
+	if (noteRepeatSwitcher === "on") { 
 
 	switch (this.id) {
 		case 'quarterNote':
-		quarterNote.style.background = "yellow";
+		quarterNote.style.background = "background";
 		eigthNote.style.background = "black";
-		sixteenthNote.style.background = "background";
+		sixteenthNote.style.background = "black";
 		thirtySecondNote.style.background = "black";
 		tempoVal = 4
 		console.log(this.id);
@@ -52,33 +52,35 @@ function setNoteDivision() {
 
 		break;
 		case 'eigthNote':
-		quarterNote.style.background = "background";
-		eigthNote.style.background = "yellow";
-		sixteenthNote.style.background = "background";
-		thirtySecondNote.style.background = "background";
+		quarterNote.style.background = "black";
+		eigthNote.style.background = "background";
+		sixteenthNote.style.background = "black";
+		thirtySecondNote.style.background = "black";;
 		tempoVal = 8;
 		console.log(this.id);
 		console.log(tempoVal);
 		break;	
 		case 'sixteenthNote':
-		sixteenthNote.style.background = "yellow";
-		eigthNote.style.background = "white";
-		sixteenthNote.style.background = "white";
-		thirtySecondNote.style.background = "white";
+		quarterNote.style.background = "black";
+		eigthNote.style.background = "black";
+		sixteenthNote.style.background = "background";
+		thirtySecondNote.style.background = "black";
 		tempoVal = 16;
 		console.log(this.id);
 		console.log(tempoVal);
 		break;	
 		case 'thirtySecondNote':
-		thirtySecondNote.style.background = "yellow";
-		eigthNote.style.background = "white";
-		sixteenthNote.style.background = "white";
-		thirtySecondNote.style.background = "white";
+		thirtySecondNote.style.background = "black";
+		eigthNote.style.background = "black";
+		sixteenthNote.style.background = "black";
+		thirtySecondNote.style.background = "background";
 		tempoVal = 32;
 		console.log(this.id);
 		console.log(tempoVal);
 		break;
 	}
+
+}
 }
 
 
@@ -305,6 +307,12 @@ function beatRepeatToggle() {
 		beatRepeatBtn.style.color = "white";
 		noteRepeatSwitcher = "off";
 
+		quarterNote.style.background = "black";
+		eigthNote.style.background = "black";
+		sixteenthNote.style.background = "black";
+		thirtySecondNote.style.background = "black";;
+		tempoVal = 8;
+
 		console.log("Note Repeat Switcher is on")
 
 		if (counterObj.newPadCounter1>0) {
@@ -377,6 +385,12 @@ function beatRepeatToggle() {
 		beatRepeatBtn.innerText = "Beat Repeat On";
 		beatRepeatBtn.style.color = "black";
 		noteRepeatSwitcher = "on";
+
+		quarterNote.style.background = "black";
+		eigthNote.style.background = "background";
+		sixteenthNote.style.background = "black";
+		thirtySecondNote.style.background = "black";;
+		tempoVal = 8;
 	}
 }
 
@@ -385,7 +399,7 @@ beatRepeatBtn.addEventListener("click", beatRepeatToggle);
 function keyToggle() {
 	if (switcher === "on") {
 		keyAssign.style.backgroundColor = "black";
-		keyAssign.innerText = "Key Assign Off";
+		document.getElementById('KeyAssignOfforOnText').innerText = "Off";
 		keyAssign.style.color = "white";
 
 		for (let x = 0; x < hiddenButton.length; x++) {
@@ -395,7 +409,7 @@ function keyToggle() {
 		switcher = "off";
 	} else {
 		keyAssign.style.backgroundColor = "yellow";
-		keyAssign.innerText = "Key Assign On";
+		document.getElementById('KeyAssignOfforOnText').innerText = "On";
 		keyAssign.style.color = "black";
 		for (let x = 0; x < hiddenButton.length; x++) {
 			hiddenButton[x].style.display = "inline"
@@ -822,6 +836,283 @@ document.addEventListener("DOMContentLoaded", function () {
 	};
 
 });
+
+var midi, data;
+if (navigator.requestMIDIAccess) {
+	navigator.requestMIDIAccess({
+		sysex: false
+	}).then(onMIDISuccess, onMIDIFailure);
+} else {
+	console.warn("No MIDI support in your browser");
+}
+function onMIDISuccess(midiData) {
+	console.log(midiData);
+	midi = midiData;
+	var allInputs = midi.inputs.values();
+	for (var input = allInputs.next(); input && !input.done; input = allInputs.next()) {
+		input.value.onmidimessage = onMIDImessage;
+	}
+}
+function onMIDIFailure() {
+	console.warn("Not finding a MIDI controller");
+}
+
+var context = new window.AudioContext;
+
+function onMIDImessage(messageData) {
+	console.log(context);
+	var gainNode = context.createGain();
+	function beatRepeat() {
+		tempo2 = document.getElementById("set-tempo").value;
+		console.log(document.getElementById("set-tempo").value);
+		noteDivision2 = tempoVal;
+		let interval = 1 / ((tempo2 / 60) * noteDivision2 / 4)
+		return interval
+	}
+
+	function playSound1(nameBufferNode, soundFile, interval) {
+		window[nameBufferNode] = context.createBufferSource();
+		var request = new XMLHttpRequest();
+		request.open('GET', soundFile, true);
+		request.responseType = 'arraybuffer';
+		request.onload = function () {
+			context.decodeAudioData(
+				request.response,
+				function (buffer) {
+					window[nameBufferNode].buffer = buffer;
+					window[nameBufferNode].connect(gainNode);
+
+					if (noteRepeatSwitcher==="on") {
+						window[nameBufferNode].loop = true;
+						console.log(interval);
+						console.log(buffer.duration);
+						window[nameBufferNode].loopEnd = interval;
+					}
+					
+					gainNode.connect(context.destination);
+					gainNode.gain.setValueAtTime(note.velocity / 127, context.currentTime);
+				},
+				function (e) { console.log("Error with decoding audio data" + e.err); }
+			);
+		};
+		request.send()
+		window[nameBufferNode].start()
+	};
+
+	function stopSound1(nameBufferNode, delay) {
+		window[nameBufferNode].stop(context.currentTime + delay);
+    };
+    
+    function playAndStop1(bufferNodeName, soundFile, pad, delay) {
+		let interval = beatRepeat();
+
+		if (switcher === "off") {
+			if (bufferNodeName === 'bufferNode1') {
+				counter = counterObj.newPadCounter1
+				counterObj.newPadCounter1 = counterObj.newPadCounter1 + 1
+			}
+			if (bufferNodeName === 'bufferNode2') {
+				counter = counterObj.newPadCounter2
+				counterObj.newPadCounter2 = counterObj.newPadCounter2 + 1
+			}
+			if (bufferNodeName === 'bufferNode3') {
+				counter = counterObj.newPadCounter3
+				counterObj.newPadCounter3 = counterObj.newPadCounter3 + 1
+			}
+			if (bufferNodeName === 'bufferNode4') {
+				counter = counterObj.newPadCounter4
+				counterObj.newPadCounter4 = counterObj.newPadCounter4 + 1
+			}
+			if (bufferNodeName === 'bufferNode5') {
+				counter = counterObj.newPadCounter5
+				counterObj.newPadCounter5 = counterObj.newPadCounter5 + 1
+			}
+			if (bufferNodeName === 'bufferNode6') {
+				counter = counterObj.newPadCounter6
+				counterObj.newPadCounter6 = counterObj.newPadCounter6 + 1
+			}
+			if (bufferNodeName === 'bufferNode7') {
+				counter = counterObj.newPadCounter7
+				counterObj.newPadCounter7 = counterObj.newPadCounter7 + 1
+			}
+			if (bufferNodeName === 'bufferNode8') {
+				counter = counterObj.newPadCounter8
+				counterObj.newPadCounter8 = counterObj.newPadCounter8 + 1
+			}
+			if (bufferNodeName === 'bufferNode9') {
+				counter = counterObj.newPadCounter9
+				counterObj.newPadCounter9 = counterObj.newPadCounter9 + 1
+			}
+			if (bufferNodeName === 'bufferNode10') {
+				counter = counterObj.newPadCounter10
+				counterObj.newPadCounter10 = counterObj.newPadCounter10 + 1
+
+			}
+			if (bufferNodeName === 'bufferNode11') {
+				counter = counterObj.newPadCounter11
+				counterObj.newPadCounter11 = counterObj.newPadCounter11 + 1
+			}
+			if (bufferNodeName === 'bufferNode12') {
+				counter = counterObj.newPadCounter12
+				counterObj.newPadCounter12 = counterObj.newPadCounter12 + 1
+
+			}
+			if (bufferNodeName === 'bufferNode13') {
+				counter = counterObj.newPadCounter13
+				counterObj.newPadCounter13 = counterObj.newPadCounter13 + 1
+			}
+			if (bufferNodeName === 'bufferNode14') {
+				counter = counterObj.newPadCounter14
+				counterObj.newPadCounter14 = counterObj.newPadCounter14 + 1
+			}
+			if (bufferNodeName === 'bufferNode15') {
+				counter = counterObj.newPadCounter15
+				counterObj.newPadCounter15 = counterObj.newPadCounter15 + 1
+
+			}
+			if (bufferNodeName === 'bufferNode16') {
+				counter = counterObj.newPadCounter16
+				counterObj.newPadCounter16 = counterObj.newPadCounter16 + 1
+			}
+
+
+
+			if (counter === 0 || noteRepeatSwitcher === "on") {
+				playSound1(bufferNodeName, soundFile, interval);
+				addBackground(pad);
+				setTimeout(removeBackground, 3, pad);
+
+			}
+			else {
+				stopSound1(bufferNodeName, delay);
+				playSound1(bufferNodeName, soundFile, interval);
+				addBackground(pad);
+				setTimeout(removeBackground, 3, pad);
+
+			}
+		}
+	};
+
+	var d = messageData.data; // Example: [144, 60, 100]
+	var note = {
+		on: d[0],
+		pitch: d[1],
+		velocity: d[2]
+	}
+
+	function play(note) {
+		switch (note.on) {
+			case 144:
+				console.log(note)
+				if (note.pitch == 0 || note.pitch == 24 || note.pitch == 48 || note.pitch == 72 || note.pitch == 96 || note.pitch == 120) {
+					playAndStop1('bufferNode1', soundObject.pad1.sound, 'pad1', 0)
+				}
+				if (note.pitch == 1 || note.pitch == 25 || note.pitch == 49 || note.pitch == 73 || note.pitch == 97) {
+					playAndStop1('bufferNode2', soundObject.pad2.sound, 'pad2', 0)
+				}
+				if (note.pitch == 2 || note.pitch == 26 || note.pitch == 50 || note.pitch == 74 || note.pitch == 98) {
+					playAndStop1('bufferNode3', soundObject.pad3.sound, 'pad3', 0)
+				}
+				if (note.pitch == 3 || note.pitch == 27 || note.pitch == 51 || note.pitch == 75 || note.pitch == 99) {
+					playAndStop1('bufferNode4', soundObject.pad4.sound, 'pad4', 0)
+				}
+				if (note.pitch == 4 || note.pitch == 28 || note.pitch == 52 || note.pitch == 76 || note.pitch == 100) {
+					playAndStop1('bufferNode5', soundObject.pad5.sound, 'pad5', 0)
+				}
+				if (note.pitch == 5 || note.pitch == 29 || note.pitch == 53 || note.pitch == 77 || note.pitch == 101) {
+					playAndStop1('bufferNode6', soundObject.pad6.sound, 'pad6', 0)
+				}
+				if (note.pitch == 6 || note.pitch == 30 || note.pitch == 54 || note.pitch == 78 || note.pitch == 102) {
+					playAndStop1('bufferNode7', soundObject.pad7.sound, 'pad7', 0)
+				}
+				if (note.pitch == 7 || note.pitch == 31 || note.pitch == 55 || note.pitch == 79 || note.pitch == 103) {
+					playAndStop1('bufferNode8', soundObject.pad8.sound, 'pad8', 0)
+				}
+				if (note.pitch == 8 || note.pitch == 32 || note.pitch == 56 || note.pitch == 80 || note.pitch == 104) {
+					playAndStop1('bufferNode9', soundObject.pad9.sound, 'pad9', 0)
+				}
+				if (note.pitch == 9 || note.pitch == 33 || note.pitch == 57 || note.pitch == 81 || note.pitch == 105) {
+					playAndStop1('bufferNode10', soundObject.pad10.sound, 'pad10', 0)
+				}
+				if (note.pitch == 10 || note.pitch == 34 || note.pitch == 58 || note.pitch == 82 || note.pitch == 106) {
+					playAndStop1('bufferNode11', soundObject.pad11.sound, 'pad11', 0)
+				}
+				if (note.pitch == 11 || note.pitch == 35 || note.pitch == 59 || note.pitch == 83 || note.pitch == 107) {
+					playAndStop1('bufferNode12', soundObject.pad12.sound, 'pad12', 0)
+				}
+				if (note.pitch == 12 || note.pitch == 36 || note.pitch == 60 || note.pitch == 84 || note.pitch == 108) {
+					playAndStop1('bufferNode13', soundObject.pad13.sound, 'pad13', 0)
+				}
+				if (note.pitch == 13 || note.pitch == 37 || note.pitch == 61 || note.pitch == 85 || note.pitch == 109) {
+					playAndStop1('bufferNode14', soundObject.pad14.sound, 'pad14', 0)
+				}
+				if (note.pitch == 14 || note.pitch == 38 || note.pitch == 62 || note.pitch == 86 || note.pitch == 110) {
+					playAndStop1('bufferNode15', soundObject.pad15.sound, 'pad15', 0)
+				}
+				if (note.pitch == 15 || note.pitch == 39 || note.pitch == 63 || note.pitch == 87 || note.pitch == 111) {
+					playAndStop1('bufferNode16', soundObject.pad16.sound, 'pad16', 0)
+				}
+
+				break;
+			case 128:
+				console.log("Off")
+				if (noteRepeatSwitcher === "on") {
+					if (note.pitch == 0 || note.pitch == 24 || note.pitch == 48 || note.pitch == 72 || note.pitch == 96 || note.pitch == 120) {
+						stopSound1('bufferNode1', 0)
+					}
+					if (note.pitch == 1 || note.pitch == 25 || note.pitch == 49 || note.pitch == 73 || note.pitch == 97) {
+						stopSound1('bufferNode2', 0)
+					}
+					if (note.pitch == 2 || note.pitch == 26 || note.pitch == 50 || note.pitch == 74 || note.pitch == 98) {
+						stopSound1('bufferNode3', 0)
+					}
+					if (note.pitch == 3 || note.pitch == 27 || note.pitch == 51 || note.pitch == 75 || note.pitch == 99) {
+						stopSound1('bufferNode4', 0)
+					}
+					if (note.pitch == 4 || note.pitch == 28 || note.pitch == 52 || note.pitch == 76 || note.pitch == 100) {
+						stopSound1('bufferNode5', 0)
+					}
+					if (note.pitch == 5 || note.pitch == 29 || note.pitch == 53 || note.pitch == 77 || note.pitch == 101) {
+						stopSound1('bufferNode6', 0)
+					}
+					if (note.pitch == 6 || note.pitch == 30 || note.pitch == 54 || note.pitch == 78 || note.pitch == 102) {
+						stopSound1('bufferNode7', 0)
+					}
+					if (note.pitch == 7 || note.pitch == 31 || note.pitch == 55 || note.pitch == 79 || note.pitch == 103) {
+						stopSound1('bufferNode8', 0)
+					}
+					if (note.pitch == 8 || note.pitch == 32 || note.pitch == 56 || note.pitch == 80 || note.pitch == 104) {
+						stopSound1('bufferNode9', 0)
+					}
+					if (note.pitch == 9 || note.pitch == 33 || note.pitch == 57 || note.pitch == 81 || note.pitch == 105) {
+						stopSound1('bufferNode10', 0)
+					}
+					if (note.pitch == 10 || note.pitch == 34 || note.pitch == 58 || note.pitch == 82 || note.pitch == 106) {
+						stopSound1('bufferNode11', 0)
+					}
+					if (note.pitch == 11 || note.pitch == 35 || note.pitch == 59 || note.pitch == 83 || note.pitch == 107) {
+						stopSound1('bufferNode12', 0)
+					}
+					if (note.pitch == 12 || note.pitch == 36 || note.pitch == 60 || note.pitch == 84 || note.pitch == 108) {
+						stopSound1('bufferNode13', 0)
+					}
+					if (note.pitch == 13 || note.pitch == 37 || note.pitch == 61 || note.pitch == 85 || note.pitch == 109) {
+						stopSound1('bufferNode14', 0)
+					}
+					if (note.pitch == 14 || note.pitch == 38 || note.pitch == 62 || note.pitch == 86 || note.pitch == 110) {
+						stopSound1('bufferNode15', 0)
+					}
+					if (note.pitch == 15 || note.pitch == 39 || note.pitch == 63 || note.pitch == 87 || note.pitch == 111) {
+						stopSound1('bufferNode16', 0)
+					}
+				}
+				break;
+		}
+	}
+
+	play(note);
+};
+
 
 
 
