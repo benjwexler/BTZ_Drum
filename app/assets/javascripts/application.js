2fe44cba2a4774befc2ca0c2d68884923905d24d
Ben Wexler <mixedywex@gmail.com>
2018-09-26 14:09:15 -0400

    Forgot to Save

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -120,24 +120,24 @@ let soundObject = {
 
 let kit1 = {
 	pad1: { sound: "/drumSamplesFolder/MetroHat1.wav", key: 49 },
-	pad2: { sound: "/drumSamplesFolder/rimshot1.wav", key: 50 },
+	pad2: { sound: "/drumSamplesFolder/rimshot1-modify.wav", key: 50 },
 	pad3: { sound: "/drumSamplesFolder/808-3.wav", key: 51 },
 	pad4: { sound: "/drumSamplesFolder/808-4-modify.wav", key: 52 },
 	pad5: { sound: "/drumSamplesFolder/808-5.wav", key: 81 },
-	pad6: { sound: "/drumSamplesFolder/clap1.wav", key: 87 },
-	pad7: { sound: "/drumSamplesFolder/clap2.wav", key: 69 },
-	pad8: { sound: "/drumSamplesFolder/clap3.wav", key: 82 },
-	pad9: { sound: "/drumSamplesFolder/clap4.wav", key: 65 },
-	pad10: { sound: "/drumSamplesFolder/crash1.wav", key: 83 },
-	pad11: { sound: "/drumSamplesFolder/hat1.wav", key: 68 },
-	pad12: { sound: "/drumSamplesFolder/hat2.wav", key: 70 },
-	pad13: { sound: "/drumSamplesFolder/hat3.wav", key: 90 },
-	pad14: { sound: "/drumSamplesFolder/hat4.wav", key: 88 },
+	pad6: { sound: "/drumSamplesFolder/clap1-modify.wav", key: 87 },
+	pad7: { sound: "/drumSamplesFolder/clap2-modify.wav", key: 69 },
+	pad8: { sound: "/drumSamplesFolder/clap3-modify.wav", key: 82 },
+	pad9: { sound: "/drumSamplesFolder/clap4-modify.wav", key: 65 },
+	pad10: { sound: "/drumSamplesFolder/crash1-modify.wav", key: 83 },
+	pad11: { sound: "/drumSamplesFolder/hat1-modify.wav", key: 68 },
+	pad12: { sound: "/drumSamplesFolder/hat2-modify.wav", key: 70 },
+	pad13: { sound: "/drumSamplesFolder/hat3-modify.wav", key: 90 },
+	pad14: { sound: "/drumSamplesFolder/hat4-modify.wav", key: 88 },
 	pad15: { sound: "/drumSamplesFolder/kick1.wav", key: 67 },
 	pad16: { sound: "/drumSamplesFolder/kick2.wav", key: 86 }
 };
 
-"/justBlazeBtz/JustBlaze-989kick.wav"
+
 
 
 
